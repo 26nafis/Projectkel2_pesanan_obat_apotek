aa6d648b96320065bec4d2b04d2b8365887b6e7a
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:21:09 +0700

    menamakan project kelompok

--- Kelompok2Project/FormAdmin.cs
+++ Kelompok2Project/FormAdmin.cs
@@ -12,60 +12,6 @@ namespace Kelompok2Project
 {
     public partial class FormAdmin : Form
     {
-        public FormAdmin()
-        {
-            InitializeComponent();
-        }
-
-        private void FormAdmin_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        // 🔥 TAMBAHAN: buka form kelola obat
-        private void btnKelolaObat_Click(object sender, EventArgs e)
-        {
-            FormObat f = new FormObat();
-            f.Show();
-        }
-
-        // 🔥 TAMBAHAN: lihat transaksi / riwayat
-        private void btnTransaksi_Click(object sender, EventArgs e)
-        {
-            FormTransaksi f = new FormTransaksi(0); // admin lihat semua
-            f.Show();
-        }
-
-        // 🔥 TAMBAHAN: logout kembali ke login
-        private void btnLogout_Click(object sender, EventArgs e)
-        {
-            FormLogin f = new FormLogin();
-            f.Show();
-            this.Close();
-        }
-
-        private void btnKelolaObat_Click_1(object sender, EventArgs e)
-        {
-            FormKelolaObat f = new FormKelolaObat();
-            f.Show();
-            this.Close();
-        }
-
-        private void btnStok_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnTransaksi_Click_1(object sender, EventArgs e)
-        {
-            FormTransaksi f = new FormTransaksi();
-            f.Show();
-            this.Close();
-        }
-
-        private void btnAkun_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
