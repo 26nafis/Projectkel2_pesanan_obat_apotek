f4a4b73f4143672d891bc0447c6acee62fa40d87
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:34:54 +0700

    TAMBAHAN: koneksi database

--- Kelompok2Project/FormRegister.cs
+++ Kelompok2Project/FormRegister.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kelompok2Project
+{
+    public partial class FormRegister : Form
+    {
+        // 🔥 TAMBAHAN: koneksi database
+        string connStr = "Data Source=NAFIS\\NAFISCOY;Initial Catalog=ApotektokoDB;Integrated Security=True";
+
+        public FormRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbRole_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+    }
+}
