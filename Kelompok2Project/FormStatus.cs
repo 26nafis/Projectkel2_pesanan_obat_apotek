74979ffcc1a693fe6ff4399beae18bd81d06de57
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:36:47 +0700

    koneksi & id user

--- Kelompok2Project/FormStatus.cs
+++ Kelompok2Project/FormStatus.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kelompok2Project
+{
+    public partial class FormStatus : Form
+    {
+        // 🔥 TAMBAHAN: koneksi & id user
+        string connStr = "Data Source=NAFIS\\NAFISCOY;Initial Catalog=ApotektokoDB;Integrated Security=True";
+        int idAkun;
+
+        public FormStatus()
+        {
+            InitializeComponent();
+        }
+
+        
+}
