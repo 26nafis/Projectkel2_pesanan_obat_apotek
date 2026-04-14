bbb1f463e8dbd7d79b0c2328efa531e367720cc7
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:30:17 +0700

    tambahan koneksi

--- Kelompok2Project/FormLogin.cs
+++ Kelompok2Project/FormLogin.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kelompok2Project
+{
+    public partial class FormLogin : Form
+    {
+        // 🔹 TAMBAHAN KONEKSI
+        string connStr = "Data Source=NAFIS\\NAFISCOY;Initial Catalog=ApotektokoDB;Integrated Security=True";
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+            {
+                
+                        
+
+                       
+                    }
+                }
+            }
+        }
+    }
+}
