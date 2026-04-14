44bcdb59d445f906fa3e5975599addbc78cd3db8
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:33:15 +0700

    LOAD DATA OBAT KE DATAGRID

--- Kelompok2Project/FormObat.cs
+++ Kelompok2Project/FormObat.cs
@@ -35,7 +35,20 @@ namespace Kelompok2Project
             LoadObat();
         }
 
-        
+        // 🔥 LOAD DATA OBAT KE DATAGRID
+        private void LoadObat()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Obat", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgvObat.DataSource = dt;
+
+            conn.Close();
+        }
 
         
 
