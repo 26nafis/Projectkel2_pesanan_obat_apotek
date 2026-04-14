e6215813e7ad125c088b811be50205cb60c69913
26nafis <spalanzaninafis@gmail.com>
2026-04-15 02:32:54 +0700

    koneksi database

--- Kelompok2Project/FormObat.cs
+++ Kelompok2Project/FormObat.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kelompok2Project
+{
+    public partial class FormObat : Form
+    {
+        // 🔥 TAMBAHAN: koneksi database
+        string connStr = "Data Source=NAFIS\\NAFISCOY;Initial Catalog=ApotektokoDB;Integrated Security=True";
+
+        int idAkun;
+
+        public FormObat()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        
+
+        
+
+       
+
+        
+    }
+}
